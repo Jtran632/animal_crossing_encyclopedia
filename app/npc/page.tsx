b548daed00104b485cade17c4a8d471bf34d76f9
jtran632 <jimmytran357@yahoo.com>
2024-08-13 01:40:00 -0700

    minor chnage

--- app/npc/page.tsx
+++ app/npc/page.tsx
@@ -49,7 +49,7 @@ async function fetchSpecialCharacters() {
     `&fields=${params.fields}` +
     `&limit=${params.limit}`;
 
-  const res = await fetch(url);
+  const res = await fetch(url, {next: {revalidate: 3600}});
   if (!res.ok) {
     throw new Error(`HTTP error! Status: ${res.status}`);
   }
